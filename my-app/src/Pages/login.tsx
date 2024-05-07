a0ce6b419d3c3efc41d7797129016855283b8005
Dhanush CK <dhanushck01@gmail.com>
2024-05-07 14:44:47 +0530

    Updated Working

--- my-app/src/Pages/login.tsx
+++ my-app/src/Pages/login.tsx
@@ -2,13 +2,13 @@ import React, {FC} from "react";
 import './login.css'
 import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-import { ITask } from "../interface";
+// import { ITask } from "../interface";
+import ToDo from "./todo";
 
  const Login: FC = () => {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    // const [taskList, setTasklist] = useState<ITask[]>([]);
     const navigate = useNavigate();
     const handleLogin = () => {
         const user = localStorage.getItem(username);
@@ -16,8 +16,8 @@ import { ITask } from "../interface";
             alert('User Not Found');
         }
         else if (JSON.parse(user).password === password) {
-            localStorage.setItem("loggedUser", JSON.stringify({user}));
-            // localStorage.setItem(user+"task", JSON.stringify([taskList, setTasklist]));
+            localStorage.setItem("loggedUser", JSON.stringify(user));
+            // setUser(user);
             navigate('/');
         }
         else {
--- my-app/src/Pages/todo.tsx
+++ my-app/src/Pages/todo.tsx
@@ -4,20 +4,29 @@ import {ITask} from "../interface";
 import ToDoTask from '../Components/ToDoTask';
 // import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 const ToDo: React.FC = () =>{
-
-  // useEffect(()=>{
-  //   const user = localStorage.getItem("loggedUser");
-  //   const tasktodo = localStorage.getItem(user+"task");
-  //   // taskt = useState<ITask[]>([]);
-  //   // [JSON.parse(tasktodo).taskList, tasktodo.setTasklist]
-  // })
-
 
   const [task, setTask] = useState<string>("");
   const [deadline, setDeadline] = useState<number>(0);
   const [todoList, setTodoList] = useState<ITask[]>([]);
+  
+  // const [user, setUser] = useState(() => {
+  //     const username = localStorage.getItem("loggedUser");
+  //     return username ? JSON.parse(username) :"";
+  // });
+
+  const user = useState(localStorage.getItem("loggedUser"));
+
+  const getStoredList = () => {
+    const List = localStorage.getItem(user+'task');
+    return List ? JSON.parse(List) : [];
+  };
+
+  useEffect(()=> {
+    // setTodoList(getStoredList());
+  })
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void  => {
     if (event.target.name === "task") {
@@ -31,6 +40,7 @@ const ToDo: React.FC = () =>{
   const addTask = (): void => {
     const newTask = { taskName: task, deadline: deadline };
     setTodoList([...todoList, newTask]);
+    localStorage.setItem(user+'task', JSON.stringify(todoList));
     setTask("");
     setDeadline(0);
   }
